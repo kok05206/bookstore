c5704a10ddebe2b152f55500224a31a9acb0a013
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2024-01-03 12:43:26 +0900

    data.sql

---
+++ data.sql
@@ -0,0 +1,11 @@
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("어린왕자", "종이책", 0, "어리다..", "많이 어리다..", "김어림", 100, "목차입니다.", 20000, "2019-01-01") ;
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("신데렐라", "종이책", 1, "유리구두..", "투명 유리구두..", "김구두", 100, "목차입니다.", 20000, "2023-12-31") ;
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("백설공주", "종이책", 2, "사과..", "빨간 사과..", "김사과", 100, "목차입니다.", 20000, "2024-01-01") ;
+
+INSERT INTO books (title, form, isbn, summary, detail, author, pages, contents, price, pub_date)
+VALUES ("흥부와 놀부", "종이책", 3, "제비..", "까만제비..", "김제비", 100, "목차입니다.", 20000, "2023-12-25") ;
