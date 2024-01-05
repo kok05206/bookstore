7c5c5ecd85fcfc9d0beac3435b6b46468a0f6906
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2024-01-05 13:04:07 +0900

    routes

--- routes/likes.js
+++ routes/likes.js
@@ -4,8 +4,8 @@ const { addLike, cancelLike } = require('../controller/LikeController');
 
 router.use(express.json());
 
-router.post('/:id', addLike); // 좋아요 추가
+router.post('/:id', addLike);
 
-router.delete('/:id', cancelLike); // 좋아요 취소
+router.delete('/:id', cancelLike);
 
 module.exports = router;
