074670dd189067f92dd1edc54f505c6b7d8b84cf
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2024-01-12 19:11:28 +0900

    feat: 장바구니 목록 삭제, 선택한 장바구니 목록

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,43 +1,69 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const addToCart = (req, res) => {
-  const { book_id, quantity, user_id } = req.body;
+  const { book_id, quantity } = req.body;
+  let authorization = ensureAuthorization(req, res);
 
-  let sql = `INSERT INTO cartItems (book_id, quantity, user_id) VALUES(?, ?, ?)`;
-  let values = [book_id, quantity, user_id];
+  if (authorization instanceof jwt.TokenExpiredError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: `로그인 세션이 만료되었습니다. 다시 로그인 해주세요.`,
+    });
+  } else if (authorization instanceof jwt.JsonWebTokenError) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: `잘못된 토큰입니다.`,
+    });
+  } else {
+    let sql = `INSERT INTO cartItems (book_id, quantity, user_id) VALUES(?, ?, ?)`;
+    let values = [book_id, quantity, authorization.id];
 
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
-    return res.status(StatusCodes.OK).json(results);
-  });
+    conn.query(sql, values, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
 };
 
 const getCartItems = (req, res) => {
-  const { user_id, selected } = req.body;
+  const { selected } = req.body;
 
-  let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price  
-  FROM cartItems LEFT JOIN books 
-  ON cartItems.book_id = books.id 
-  WHERE user_id = ? AND cartItems.id IN (?)`;
-  let values = [user_id, selected];
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
-    return res.status(StatusCodes.OK).json(results);
-  });
+  let authorization = ensureAuthorization(req, res);
+
+  if (authorization instanceof jwt.TokenExpiredError) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: `로그인 세션이 만료되었습니다. 다시 로그인 해주세요.`,
+    });
+  } else if (authorization instanceof jwt.JsonWebTokenError) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: `잘못된 토큰입니다.`,
+    });
+  } else {
+    let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price  
+              FROM cartItems LEFT JOIN books 
+              ON cartItems.book_id = books.id 
+              WHERE user_id = ? AND cartItems.id IN (?)`;
+    let values = [authorization.id, selected];
+    conn.query(sql, values, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(StatusCodes.BAD_REQUEST).end();
+      }
+      return res.status(StatusCodes.OK).json(results);
+    });
+  }
 };
 
 const removeCartItem = (req, res) => {
-  const { id } = req.params;
+  const cartItemId = req.params.id;
 
   let sql = `DELETE FROM cartItems WHERE id = ?`;
-  conn.query(sql, id, (err, results) => {
+  conn.query(sql, cartItemId, (err, results) => {
     if (err) {
       console.log(err);
       return res.status(StatusCodes.BAD_REQUEST).end();
@@ -45,5 +71,22 @@ const removeCartItem = (req, res) => {
     return res.status(StatusCodes.OK).json(results);
   });
 };
+
+function ensureAuthorization(req, res) {
+  try {
+    let receivedJwt = req.headers['authorization'];
+    console.log('receivedJwt : ', receivedJwt);
+
+    let decodedUserInfo = jwt.verify(receivedJwt, process.env.PRIVATE_KEY);
+    console.log(decodedUserInfo);
+
+    return decodedUserInfo;
+  } catch (err) {
+    console.log(err.name);
+    console.log(err.message);
+
+    return err;
+  }
+}
 
 module.exports = { addToCart, getCartItems, removeCartItem };
